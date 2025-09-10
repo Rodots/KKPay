524df8ffb433c94c3c0e7650afc202c42bb8673a
Rodots <admin@h2fast.cn>
2025-09-10 15:03:51 +0800

    feat(OrderController): 优化订单搜索功能并添加新筛选条件

--- app/admin/controller/OrderController.php
+++ app/admin/controller/OrderController.php
@@ -1,11 +1,14 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace app\admin\controller;
 
 use app\model\Order;
+use app\model\PaymentChannel;
+use app\model\PaymentChannelAccount;
 use Core\baseController\AdminBase;
+use Exception;
 use support\Db;
 use support\Request;
 use support\Response;
@@ -20,19 +23,48 @@ class OrderController extends AdminBase
     {
         $from   = $request->get('from', 0);
         $limit  = $request->get('limit', 10);
-        $params = $request->only(['fuzzy_trade_no', 'trade_no', 'out_trade_no', 'api_trade_no', 'bill_trade_no', 'merchant_number', 'payment_type', 'payment_channel_account_id', 'subject', 'total_amount', 'receipt_amount', 'buyer_pay_amount', 'create_time', 'payment_time', 'trade_state', 'settle_state', 'notify_state']);
+        $params = $request->only(['fuzzy_trade_no', 'trade_no', 'out_trade_no', 'api_trade_no', 'bill_trade_no', 'merchant_number', 'payment_type', 'payment_channel_code', 'payment_channel_account_id', 'subject', 'total_amount', 'buyer_pay_amount', 'receipt_amount', 'create_time', 'payment_time', 'trade_state', 'settle_state', 'notify_state']);
 
         try {
             validate([
-                'account'    => 'max:32',
-                'nickname'   => 'max:16',
-                'email'      => 'max:64',
-                'created_at' => 'array'
+                'fuzzy_trade_no'             => ['max:256', 'alphaDash'],
+                'trade_no'                   => ['max:28', 'alphaDash'],
+                'out_trade_no'               => ['max:128', 'alphaDash'],
+                'api_trade_no'               => ['max:256', 'alphaDash'],
+                'bill_trade_no'              => ['max:256', 'alphaDash'],
+                'merchant_number'            => ['alphaNum', 'startWith:M', 'length:24'],
+                'payment_channel_code'       => ['max:16', 'alphaNum', 'upper'],
+                'payment_channel_account_id' => ['number'],
+                'subject'                    => ['max:255'],
+                'total_amount'               => ['float'],
+                'buyer_pay_amount'           => ['float'],
+                'receipt_amount'             => ['float'],
+                'create_time'                => ['array'],
+                'payment_time'               => ['array']
             ], [
-                'account.max'      => '账号长度不能超过32位',
-                'nickname.max'     => '昵称长度不能超过16位',
-                'email.max'        => '邮箱长度不能超过64位',
-                'created_at.array' => '请重新选择选择时间范围'
+                'fuzzy_trade_no.max'                => '四合一单号长度不能超过256位',
+                'fuzzy_trade_no.alphaDash'          => '四合一单号只能是字母和数字，下划线及破折号',
+                'trade_no.max'                      => '平台订单号长度不能超过256位',
+                'trade_no.alphaDash'                => '平台订单号只能是字母和数字，下划线及破折号',
+                'out_trade_no.max'                  => '商户订单号长度不能超过256位',
+                'out_trade_no.alphaDash'            => '商户订单号只能是字母和数字，下划线及破折号',
+                'api_trade_no.max'                  => '上游订单号长度不能超过256位',
+                'api_trade_no.alphaDash'            => '上游订单号只能是字母和数字，下划线及破折号',
+                'bill_trade_no.max'                 => '交易流水号长度不能超过256位',
+                'bill_trade_no.alphaDash'           => '交易流水号只能是字母和数字，下划线及破折号',
+                'merchant_number.alphaNum'          => '商户编号是以M开头的24位英文+数字',
+                'merchant_number.startWith'         => '商户编号是以M开头的24位英文+数字',
+                'merchant_number.length'            => '商户编号是以M开头的24位英文+数字',
+                'payment_channel_code.max'          => '支付通道编码长度不能超过16位',
+                'payment_channel_code.alphaNum'     => '支付通道编码只能是大写字母和数字',
+                'payment_channel_code.upper'        => '支付通道编码只能是大写字母和数字',
+                'payment_channel_account_id.number' => '支付通道账户ID只能是数字',
+                'subject:max'                       => '商品名称长度不能超过255位',
+                'total_amount.float'                => '订单总金额格式不正确',
+                'buyer_pay_amount.float'            => '用户付款金额格式不正确',
+                'receipt_amount.float'              => '商户实收金额格式不正确',
+                'create_time.array'                 => '请重新选择选择时间范围',
+                'payment_time.array'                => '请重新选择选择时间范围'
             ])->check($params);
         } catch (Throwable $e) {
             return $this->fail($e->getMessage());
@@ -45,17 +77,65 @@ class OrderController extends AdminBase
                     continue;
                 }
                 switch ($key) {
-                    case 'account':
-                        $q->where('account', $value);
+                    case 'fuzzy_trade_no':
+                        $q->orWhere(function ($query) use ($value) {
+                            $value = trim($value);
+                            $query->where('trade_no', $value)
+                                ->where('out_trade_no', $value)
+                                ->where('api_trade_no', $value)
+                                ->where('bill_trade_no', $value);
+                        });
                         break;
-                    case 'email':
-                        $q->where('email', 'like', "%$value%");
+                    case 'trade_no':
+                        $q->where('trade_no', 'like', "%$value%");
                         break;
-                    case 'status':
-                        $q->where('status', (int)$value);
+                    case 'out_trade_no':
+                        $q->where('out_trade_no', 'like', "%$value%");
                         break;
-                    case 'created_at':
-                        $q->whereBetween('created_at', [$value[0], $value[1]]);
+                    case 'api_trade_no':
+                        $q->where('api_trade_no', 'like', "%$value%");
+                        break;
+                    case 'bill_trade_no':
+                        $q->where('bill_trade_no', 'like', "%$value%");
+                        break;
+                    case 'merchant_number':
+                        $q->where('merchant_number', trim($value));
+                        break;
+                    case 'payment_type':
+                        $q->where('payment_type', $value);
+                        break;
+                    case 'payment_channel_code':
+                        $q->whereIn('payment_channel_account_id', PaymentChannelAccount::where('payment_channel_id', PaymentChannel::where('code', $value)->value('id'))->pluck('id'));
+                        break;
+                    case 'payment_channel_account_id':
+                        $q->where('payment_channel_account_id', $value);
+                        break;
+                    case 'subject':
+                        $q->where('subject', 'like', "%$value%");
+                        break;
+                    case 'total_amount':
+                        $q->where('total_amount', (float)$value);
+                        break;
+                    case 'receipt_amount':
+                        $q->where('receipt_amount', (float)$value);
+                        break;
+                    case 'buyer_pay_amount':
+                        $q->where('buyer_pay_amount', (float)$value);
+                        break;
+                    case 'create_time':
+                        $q->whereBetween('create_time', [$value[0], $value[1]]);
+                        break;
+                    case 'payment_time':
+                        $q->whereBetween('payment_time', [$value[0], $value[1]]);
+                        break;
+                    case 'trade_state':
+                        $q->where('trade_state', $value);
+                        break;
+                    case 'settle_state':
+                        $q->where('settle_state', $value);
+                        break;
+                    case 'notify_state':
+                        $q->where('notify_state', (bool)$value);
                         break;
                 }
             }
@@ -110,7 +190,7 @@ class OrderController extends AdminBase
                 $order = Order::find($trade_no);
                 $order->refundProcessing((float)$amount);
             });
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return $this->fail($e->getMessage());
         }
 
