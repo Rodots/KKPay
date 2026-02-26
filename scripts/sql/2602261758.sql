f5cb6cca80601a4198ec38e26bf701cca0d87978
Rodots <admin@h2fast.cn>
2026-02-26 17:59:10 +0800

    feat(database): 更新买家最小年龄字段

--- scripts/sql/2602261758.sql
+++ scripts/sql/2602261758.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `kkpay_merchant`
 ADD COLUMN `nickname` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商户昵称' AFTER `merchant_number`;
+
+ALTER TABLE `kkpay_order_buyer`
+MODIFY COLUMN `min_age` tinyint NOT NULL DEFAULT 0 COMMENT '最小年龄';
